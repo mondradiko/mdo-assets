80d43102ee31a2d9df9fdd69ec31dd3c13ab55bb
marceline-cramer <cramermarceline@gmail.com>
2021-05-16 20:52:51 -0600

    Add tests/ and asset_ctx test

---
+++ tests/asset_ctx.c
@@ -0,0 +1,10 @@
+#include "test_common.h"
+
+#include "src/asset_ctx.h"
+
+Test (asset_ctx, init_and_destroy)
+{
+  mdo_asset_ctx_t ctx;
+  cr_assert_eq (0, mdo_asset_ctx_init (&ctx, cr_allocator ()));
+  mdo_asset_ctx_destroy (&ctx);
+}
---
+++ tests/test_common.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <criterion/criterion.h>
+
+#include "src/allocator.h"
+
+const mdo_allocator_t *cr_allocator ();
