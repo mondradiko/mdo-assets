d9c5fcb77ac96cd896705c42078245e69f5275a5
marceline-cramer <cramermarceline@gmail.com>
2021-05-16 21:14:09 -0600

    Document asset_ctx methods

--- include/asset_ctx.h
+++ include/asset_ctx.h
@@ -15,5 +15,18 @@
 typedef struct mdo_asset_ctx_s mdo_asset_ctx_t;
 
 /* functions */
+
+/*! @function
+  @brief Initializes an asset_ctx.
+  @param ctx The asset_ctx to initialize.
+  @param alloc An allocator to use, or NULL for the default.
+  @return Zero for success, non-zero otherwise.
+ */
 int mdo_asset_ctx_init (mdo_asset_ctx_t *, const mdo_allocator_t *);
+
+/*! @function
+  @brief Destroys an asset_ctx.
+  @param ctx The asset_ctx to destroy.
+  @return The time that the event loop spent in idle, in nanoseconds.
+ */
 uint64_t mdo_asset_ctx_destroy (mdo_asset_ctx_t *);
