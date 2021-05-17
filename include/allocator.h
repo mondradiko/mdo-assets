e8c5f8748ae97ec7217b12f1113a89cc91fecd53
marceline-cramer <cramermarceline@gmail.com>
2021-05-16 21:13:12 -0600

    Add documented mdo_allocator_t methods

--- include/allocator.h
+++ include/allocator.h
@@ -5,8 +5,49 @@
 
 #pragma once
 
+#include <stddef.h>
+
 /* public types */
 typedef struct mdo_allocator_s mdo_allocator_t;
 
 /* functions */
+
+/*! @function
+  @brief Allocates a block of memory from an allocator.
+  @param alloc The allocator to allocate from.
+  @param size The size of the memory to allocate.
+  @return A pointer to the freshly allocated memory, or NULL on failure.
+ */
+void *mdo_allocator_malloc (const mdo_allocator_t *, size_t);
+
+/*! @function
+  @brief Allocates an array of elements from an allocator.
+  @param alloc The allocator to allocate from.
+  @param num The number of elements to allocate.
+  @param size The size of each element.
+  @return A pointer to the freshly allocated array, or NULL on failure.
+ */
+void *mdo_allocator_calloc (const mdo_allocator_t *, size_t, size_t);
+
+/*! @function
+  @brief Re-allocates memory from an allocator.
+  @param alloc The allocator to re-allocate from.
+  @param memory A pointer to the original memory.
+  @param size The new size of the data.
+  @return A pointer to the re-allocated data, or NULL on failure.
+ */
+void *mdo_allocator_realloc (const mdo_allocator_t *, void *, size_t);
+
+/*! @function
+  @brief Frees memory from an allocator.
+  @param alloc The allocator to free from.
+  @param memory The memory to free.
+ */
+void mdo_allocator_free (const mdo_allocator_t *, void *);
+
+/*! @function
+  @brief Retrieves the default allocator. For now, uses stdlib malloc(),
+  free(), etc.
+  @return A handle to the global default allocator.
+ */
 const mdo_allocator_t *mdo_default_allocator ();
